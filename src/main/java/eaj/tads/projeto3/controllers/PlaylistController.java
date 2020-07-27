de6d08963c51d437413d92370c5d0799222a651d
Darkisda <luandesouzanascimento2011@gmail.com>
2020-07-27 10:09:36 -0300

    playlist methods ok

--- src/main/java/eaj/tads/projeto3/controllers/PlaylistController.java
+++ src/main/java/eaj/tads/projeto3/controllers/PlaylistController.java
@@ -3,9 +3,11 @@ package eaj.tads.projeto3.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +39,21 @@ public class PlaylistController {
     public ResponseEntity<Playlist> insert(@RequestBody Playlist playlist) {
         service.insert(playlist);
         return ResponseEntity.status(201).body(playlist);
+    }
+
+    @PutMapping(value = { "/{id}" })
+    public ResponseEntity<Playlist> update(@PathVariable Long id, @RequestBody Playlist playlist) {
+        return service.findById(id).map(record -> {
+            service.saveAndFlush(playlist);
+            return ResponseEntity.ok().body(playlist);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        return service.findById(id).map(record -> {
+            service.delete(record);
+            return ResponseEntity.ok().build();
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
